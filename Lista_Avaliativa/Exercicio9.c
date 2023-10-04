5131f0536f588e347572e988265da98e29ec4c64
Vitória Albuquerque <vitoriacordeirodf@gmail.com>
2023-10-04 15:11:18 -0300

    Update Exercicio9.c

--- Lista_Avaliativa/Exercicio9.c
+++ Lista_Avaliativa/Exercicio9.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#define TAM_VOTOS 6
 
 int main(){
 
@@ -15,6 +16,27 @@ int main(){
     // - total de votos em branco;
     // Como finalizador do conjunto de votos, tem-se o valor 0.
 
+
+  char tipo_voto[][14] = {"Candidato1", "Candidato2", "Candidato3", "Candidato4", "Votos nulos", "Votos brancos"};
+  int votos[TAM_VOTOS] = {0, 0, 0, 0, 0, 0};
+  int i, voto;
+
+  printf("Entre com uma sequência de votos terminada pelo número 0\n");
+  voto = 1;
+
+  while(voto != 0){
+    scanf("%d", &voto);
+    
+    if(voto >= 1 && voto <= TAM_VOTOS) 
+      votos[voto - 1]++;
+  }
+ 
+   
+  puts("-----Contagem dos votos-------");
+  for(i=0; i < TAM_VOTOS; i++){
+    printf("%s = %d\n", tipo_voto[i], votos[i]);
+  }
+    
 
 
 
