3b76b10319e9b52a91ef303aec3d0871ad49aef1
Vitória Albuquerque <vitoriacordeirodf@gmail.com>
2023-10-29 16:00:36 -0300

    Update Exercicio5.c

--- Lista_12_Funções/Exercicio5.c
+++ Lista_12_Funções/Exercicio5.c
@@ -15,13 +15,13 @@ void calcularIdade(int diaNascimento, int mesNascimento, int anoNascimento, int 
     nascimento.tm_hour = 0;
     nascimento.tm_min = 0;
     nascimento.tm_sec = 0;
-    nascimento.tm_isdst = -1;  // Determinar se o horário de verão está em vigor
+    nascimento.tm_isdst = -1;  
 
     double diferenca = difftime(t, mktime(&nascimento));
 
-    *dias = diferenca / (60 * 60 * 24); // Um dia tem 60*60*24 segundos
+    *dias = diferenca / (60 * 60 * 24); 
     *anos = *dias / 365;
-    *meses = (*dias % 365) / 30; // Estimativa de 30 dias por mês
+    *meses = (*dias % 365) / 30; 
     *dias = *dias - (*anos * 365) - (*meses * 30);
 }
 
@@ -29,14 +29,14 @@ int main() {
     int diaNascimento, mesNascimento, anoNascimento;
     int dias, meses, anos;
 
-    // Solicita a data de nascimento
+   
     printf("Digite a data de nascimento (DD MM AAAA): ");
     scanf("%d %d %d", &diaNascimento, &mesNascimento, &anoNascimento);
 
-    // Chame a função para calcular a idade
+    
     calcularIdade(diaNascimento, mesNascimento, anoNascimento, &dias, &meses, &anos);
 
-    // Exibe a idade em dias, meses e anos
+   
     printf("Idade: %d anos, %d meses, %d dias\n", anos, meses, dias);
 
     return 0;
