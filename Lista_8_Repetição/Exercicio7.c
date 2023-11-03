b320c30c53352c65a425f140ebd13113045f04fe
Vitória Albuquerque <vitoriacordeirodf@gmail.com>
2023-11-03 13:24:51 -0300

    Update Exercicio7.c

--- Lista_8_Repetição/Exercicio7.c
+++ Lista_8_Repetição/Exercicio7.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <ctype.h>
 
 int main(){
 
@@ -15,9 +15,56 @@ int main(){
     //e o peso médio do grupo
 
 
+    char nome[100];
+    char sexo;
+    float altura, peso;
+    int numHomens = 0, numMulheres = 0;
+    float somaAlturaHomens = 0, somaAlturaMulheres = 0, somaAlturaGrupo = 0;
+    float somaPesoHomens = 0, somaPesoMulheres = 0, somaPesoGrupo = 0;
 
+    for (int i = 0; i < 10; i++) {
+        printf("\nDigite o nome da pessoa %d: ", i + 1);
+        scanf(" %[^\n]s", nome);
 
+        printf("Digite o sexo (M/m para masculino, F/f para feminino): ");
+        scanf(" %c", &sexo);
 
+        printf("Digite a altura (em metros): ");
+        scanf("%f", &altura);
+
+        printf("Digite o peso (em kg): ");
+        scanf("%f", &peso);
+
+        if (tolower(sexo) == 'm') {
+            numHomens++;
+            somaAlturaHomens += altura;
+            somaPesoHomens += peso;
+        } else if (tolower(sexo) == 'f') {
+            numMulheres++;
+            somaAlturaMulheres += altura;
+            somaPesoMulheres += peso;
+        }
+
+        somaAlturaGrupo += altura;
+        somaPesoGrupo += peso;
+    }
+
+    float alturaMediaHomens = somaAlturaHomens / numHomens;
+    float alturaMediaMulheres = somaAlturaMulheres / numMulheres;
+    float alturaMediaGrupo = somaAlturaGrupo / 10;
+    float pesoMediaHomens = somaPesoHomens / numHomens;
+    float pesoMediaMulheres = somaPesoMulheres / numMulheres;
+    float pesoMediaGrupo = somaPesoGrupo / 10;
+
+    printf("\n-------------------------ESTATISTICAS-------------------------\n");
+    printf("Numero de homens: %d\n", numHomens);
+    printf("Numero de mulheres: %d\n", numMulheres);
+    printf("Altura media dos homens: %.2f metros\n", alturaMediaHomens);
+    printf("Altura media das mulheres: %.2f metros\n", alturaMediaMulheres);
+    printf("Altura edia do grupo: %.2f metros\n", alturaMediaGrupo);
+    printf("Peso medio dos homens: %.2f kg\n", pesoMediaHomens);
+    printf("Peso medio das mulheres: %.2f kg\n", pesoMediaMulheres);
+    printf("Peso medio do grupo: %.2f kg\n", pesoMediaGrupo);
 
     return 0;
 }
