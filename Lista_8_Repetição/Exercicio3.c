2133d19467d3563d7652e35c74c7b8f54b7e33eb
Vitória Albuquerque <vitoriacordeirodf@gmail.com>
2023-11-03 12:49:22 -0300

    Update Exercicio3.c

--- Lista_8_Repetição/Exercicio3.c
+++ Lista_8_Repetição/Exercicio3.c
@@ -8,11 +8,37 @@ int main(){
     // dois termos anteriores. Alguns termos dessa sequência são: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
 
 
-    
+    int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        int a = 0, b = 1, temp;
+        for (int i = 2; i <= n; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
 
+int main() {
+    int n;
 
+    printf("Digite o valor de n (numero inteiro maior ou igual a zero): ");
+    scanf("%d", &n);
 
-
+    if (n < 0) {
+        printf("Por favor, insira um numero inteiro maior ou igual a zero.\n");
+    } else {
+        int resultado = fibonacci(n);
+        printf("O %d termo da sequencia de Fibonacci e: %d\n", n, resultado);
+    }
 
     return 0;
 }
+
+
+
+
+
